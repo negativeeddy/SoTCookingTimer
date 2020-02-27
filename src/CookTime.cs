b5ef002ce01c8b7c3df905f4ac743234f01c02b6
Ben Williams <benwilli@microsoft.com>
2020-02-27 16:40:21 -0600

    add multiple image capability

--- src/CookTime.cs
+++ src/CookTime.cs
@@ -3,7 +3,7 @@
     public class CookTime
     {
         public string Name { get; set; }
-        public string Image { get; set; }
+        public string[] Images { get; set; }
         public int UndercookedSeconds { get; set; }
         public int CookedSeconds { get; set; }
         public int BurnedSeconds { get; set; }
@@ -13,7 +13,7 @@
             new CookTime
             {
                 Name = "Fish",
-                Image = "Fish_Ruby_SplashTail_Small.png",
+                Images = new [] { "Fish_Ruby_SplashTail_Small.png", "Fish_Olive_PlentiFin_Small.png"  },
                 UndercookedSeconds = 30,
                 CookedSeconds = 40,
                 BurnedSeconds = 80,
@@ -21,7 +21,7 @@
             new CookTime
             {
                 Name = "Trophy Fish",
-                Image = "Fish_Ruby_SplashTail.png",
+                Images = new [] { "Fish_Ruby_SplashTail.png"} ,
                 UndercookedSeconds = 80,
                 CookedSeconds = 90,
                 BurnedSeconds = 180,
@@ -29,7 +29,7 @@
             new CookTime
             {
                 Name = "Animal meat",
-                Image = "Food_Bacon.png",
+                Images = new [] {"Food_Bacon.png", "Chicken.png" },
                 UndercookedSeconds = 50,
                 CookedSeconds = 60,
                 BurnedSeconds = 120,
@@ -37,7 +37,7 @@
             new CookTime
             {
                 Name = "Monster meat",
-                Image = "Kraken_Meat.png",
+                Images = new [] { "Kraken_Meat.png", "Meg_Meat.png" },
                 UndercookedSeconds = 100,
                 CookedSeconds = 120,
                 BurnedSeconds = 240,
